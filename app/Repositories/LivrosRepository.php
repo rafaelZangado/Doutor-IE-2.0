fe1095fb855bd03c66b809a422ac280c6329a75b
rafaelZangado <fael.andrade0@gmail.com>
2024-06-30 21:50:11 -0300

    adiciona comentarios nos metodos do repository baseado nas PSR-4

--- app/Repositories/LivrosRepository.php
+++ app/Repositories/LivrosRepository.php
@@ -14,12 +14,25 @@
 
         }
 
+        /**
+         * Create a new livro record.
+         *
+         * @param  array  $dados
+         * @return Livro
+         */
         public function create(array $dados)
         {
             $dados['usuario_publicador_id'] =  Auth::user()->id;
             return $this->livroModel->create($dados);
         }
 
+        /**
+         * Get all livros with optional filtering by tituloLivro and tituloIndice.
+         *
+         * @param  string|null  $tituloLivro
+         * @param  string|null  $tituloIndice
+         * @return \Illuminate\Database\Eloquent\Collection
+         */
         public function getAllWithFilters($tituloLivro = null, $tituloIndice = null)
         {
             $query = $this->livroModel->query();
@@ -37,12 +50,26 @@
             return $query->with('indices')->get();
         }
 
+        /**
+         * Update an existing livro record.
+         *
+         * @param  array  $dados
+         * @param  int  $id
+         * @return Livro
+         */
         public function update(array $dados, int $id)
         {
             $livro = $this->livroModel->findOrFail($id);
             $livro->update($dados);
             return $livro;
         }
+
+        /**
+         * Delete a livro record.
+         *
+         * @param  int  $id
+         * @return Livro
+         */
         public function delete(int $id)
         {
             $livro = $this->livroModel->findOrFail($id);
@@ -50,11 +77,22 @@
             return $livro;
         }
 
+        /**
+         * Get all livros.
+         *
+         * @return \Illuminate\Database\Eloquent\Collection
+         */
         public function list()
         {
             return $this->livroModel->all();
         }
 
+        /**
+         * Find a livro by ID.
+         *
+         * @param  int  $id
+         * @return Livro
+         */
         public function find(int $id)
         {
             return $this->livroModel->findOrFail($id);
